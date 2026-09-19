07072a345e573f96b24222f8039e26703f0a54b6
AndersonDuranH <anderson_duran@ucne.edu.do>
2026-09-18 21:24:54 -0400

    feat: adaptar registro a estudiantes

--- RegistroLibro/Program.cs
+++ RegistroLibro/Program.cs
@@ -4,36 +4,26 @@ using RegistroLibro.Context;
 using RegistroLibro.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var ConStr = builder.Configuration.GetConnectionString("ConStr");
-
-builder.Services.AddDbContextFactory<Contexto>(options =>
-    options.UseSqlite(ConStr));
-
-builder.Services.AddScoped<LibrosServices>();
-
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+var conStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlite(conStr));
+builder.Services.AddScoped<EstudiantesServices>();
 builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<Contexto>().Database.Migrate();
+}
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
